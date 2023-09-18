343e77b6a46bf5174b37f3674438f56a0df51254
Ember Chow <emberchow.businiess@iu.edu>
2023-09-18 15:15:20 -0700

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,9 @@ copyright = '2023, Ember "Emmy" Chow'
 author = 'Ember "Emmy" Chow'
 
 # The short X.Y version
-version = '0.1.0'
+version = '1.0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
